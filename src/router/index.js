89aa447328beaf75b74436cf62fb346617706871
xielingxiao <274105278@qq.com>
2018-05-16 18:03:00 +0800

    'init'

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+/*
+路由器对象模块
+*/
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
+Vue.use(VueRouter)
+export default new VueRouter({
+  routes: [
+    {
+      path: '/msite',
+      component: MSite,
+      meta: {
+        isShow: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        isShow: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        isShow: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        isShow: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
